8c748ef0517cb6a1a7352aeed3bd46b929de4a24
pricilamina9213 <pricila.mina73363@ucaldas.edu.co>
2026-06-05 07:43:56 -0500

    agrego ejercicio Automovil

---
+++ src/Automovil.java
@@ -0,0 +1,13 @@
+public class Automovil {
+    public String marca= "Ford" ;
+    public String color ;
+    public String modelo;
+    public boolean encendido;
+
+public void ImprimirInformacionVehiculo(String marca){
+    System.out.println("Marca:"+ marca);
+}
+public void ImprimirInformacionVehiculoV2(String marca){
+    System.out.println("Marca:"+ this.marca);
+}
+}
